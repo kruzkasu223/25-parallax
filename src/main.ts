c7af621fd7ae2cb5470583c8c793c5fa2b1d8bd7
Krushang Kasundra <kruzkasu223@gmail.com>
2023-02-26 16:02:26 +0530

    feat: done

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,11 @@
 import "./style.css"
 
-console.log("25/27 - Parallax")
+const textMain = document.querySelector("section.one .text_main") as HTMLElement
+const textStroke = document.querySelector(
+  "section.one .text_stroke"
+) as HTMLElement
+
+document.addEventListener("scroll", () => {
+  textMain.style.left = Math.max(100 - 0.05 * window.scrollY, -100) + "%"
+  textStroke.style.left = Math.max(100 - 0.05 * window.scrollY, -100) + "%"
+})
